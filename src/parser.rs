156c6f524ba8ae55f547a6b3f56f47d12b61fcdf
Gabriele Rastello <grastello@localhost.localdomain>
2019-07-27 10:11:25 +0200

    Add parselets for operators.

--- src/parser.rs
+++ src/parser.rs
@@ -3,14 +3,33 @@ use crate::token::*;
 
 /// The parser.
 pub struct Parser {
+    /// Tokens to parse.
     tokens: Vec<Token>,
+    /// Index of current token.
     current: usize,
+    // Left parselets (led).
+    // consequent_parselets: Vec<Box<ConsequentParselet>>,
 }
 
 impl Parser {
     /// Make a new parser.
     pub fn new(tokens: Vec<Token>) -> Self {
         Parser { tokens, current: 0 }
+    }
+
+    /// Dispatch a consequent parselet.
+    fn get_consequent_parselet(&self, token: &Token) -> Result<Box<ConsequentParselet>, ParseError> {
+        match token.kind {
+            TokenKind::Plus => Ok(Box::new(OperatorParselet::new(TokenKind::Plus))),
+            TokenKind::Minus => Ok(Box::new(OperatorParselet::new(TokenKind::Minus))),
+            TokenKind::Star => Ok(Box::new(OperatorParselet::new(TokenKind::Star))),
+            TokenKind::Slash => Ok(Box::new(OperatorParselet::new(TokenKind::Slash))),
+            TokenKind::Cap => Ok(Box::new(OperatorParselet::new(TokenKind::Cap))),
+            _ => Err(ParseError::new(
+                "no parselet found for this token (maybe this is not an operator?)",
+                token,
+            )),
+        }
     }
 
     /// Advance the parser retrieving a new token.
@@ -32,7 +51,7 @@ impl Parser {
     }
 
     /// The actual parsing function.
-    fn actually_parse(&mut self, last_operator_binding_power: u8) -> Result<Expr, ParseError> {
+    fn actually_parse(&mut self, current_binding_power: u8) -> Result<Expr, ParseError> {
         // Check that the first token is a number.
         let first_token = self.advance();
         if !first_token.is_number() {
@@ -41,41 +60,89 @@ impl Parser {
         let mut left_expr = Expr::Literal(Literal::new(first_token.clone()));
 
         loop {
-            let next_token = self.peek();
-
-            // If the next token is an operator and binds strongly than the
-            // previous one parse after it otherwise exit the loop.
-            //
-            // We grow the expression to the right as far as we can find
-            // operators that bind at least as strongly as the previus one and
-            // we grow the expression to the left (by exiting the loop thus
-            // "lifting") when this is impossible.
-            if next_token.is_operator() {
-                if next_token.binding_power() > last_operator_binding_power {
-                    let operator = self.advance().clone();
-
-                    // If the operator is right associative we reduce its
-                    // binding power by 1 so that we keep recursing if we
-                    // hit the same operator again.
-                    let operator_binding_power = if operator.is_left_associative() {
-                        operator.binding_power()
-                    } else {
-                        operator.binding_power() - 1
-                    };
-                    let rigth_expr = self.actually_parse(operator_binding_power)?;
-                    
-                    left_expr = Expr::Binary(Binary::new(operator, left_expr, rigth_expr));
-                } else {
-                    break;
-                }
-            } else if let TokenKind::Eof = next_token.kind {
+            let current_token = self.peek().clone();
+            if let TokenKind::Eof = current_token.kind {
                 break;
-            } else {
-                return Err(ParseError::new("expected operator", next_token));
             }
+
+            let consequent_parselet = self.get_consequent_parselet(&current_token)?;
+            if consequent_parselet.binding_power() <= current_binding_power {
+                break;
+            }
+
+            self.advance();
+            left_expr = consequent_parselet.parse(self, current_token, left_expr)?;
         }
 
         Ok(left_expr)
+    }
+}
+
+/// Trait for consequent parselets.
+trait ConsequentParselet {
+    /// Return the operator's binding power.
+    fn binding_power(&self) -> u8;
+    /// The parse function.
+    fn parse(
+        &self,
+        parser: &mut Parser,
+        current_token: Token,
+        left_expr: Expr,
+    ) -> Result<Expr, ParseError>;
+}
+
+/// Operator parselet.
+struct OperatorParselet {
+    /// Binding power of the operator.
+    binding_power: u8,
+    /// True if the operator is left associative.
+    left_associative: bool,
+}
+
+impl OperatorParselet {
+    /// Make a new operator parselet from an operator.
+    fn new(operator: TokenKind) -> Self {
+        match operator {
+            TokenKind::Plus | TokenKind::Minus => OperatorParselet {
+                binding_power: 10,
+                left_associative: true,
+            },
+            TokenKind::Star | TokenKind::Slash => OperatorParselet {
+                binding_power: 20,
+                left_associative: true,
+            },
+            TokenKind::Cap => OperatorParselet {
+                binding_power: 30,
+                left_associative: false,
+            },
+            _ => panic!("Can't make an operator parselet from a non-operator"),
+        }
+    }
+}
+
+impl ConsequentParselet for OperatorParselet {
+    fn binding_power(&self) -> u8 {
+        self.binding_power
+    }
+
+    fn parse(
+        &self,
+        parser: &mut Parser,
+        current_token: Token,
+        left_expr: Expr,
+    ) -> Result<Expr, ParseError> {
+        let binding_power = if self.left_associative {
+            self.binding_power
+        } else {
+            self.binding_power - 1
+        };
+        let right_expr = parser.actually_parse(binding_power)?;
+
+        Ok(Expr::Binary(Binary::new(
+            current_token,
+            left_expr,
+            right_expr,
+        )))
     }
 }
 
--- src/token.rs
+++ src/token.rs
@@ -10,42 +10,11 @@ impl Token {
         Token { kind, lexeme }
     }
 
-    /// Return true if the Token is an operator.
-    pub fn is_operator(&self) -> bool {
-        match self.kind {
-            TokenKind::Minus
-            | TokenKind::Plus
-            | TokenKind::Slash
-            | TokenKind::Star
-            | TokenKind::Cap => true,
-            _ => false,
-        }
-    }
-
     /// Return true if the Token is a number.
     pub fn is_number(&self) -> bool {
         match self.kind {
             TokenKind::Number(_) => true,
             _ => false,
-        }
-    }
-
-    /// Return the binding power of an operator.
-    pub fn binding_power(&self) -> u8 {
-        match self.kind {
-            TokenKind::Minus | TokenKind::Plus => 10,
-            TokenKind::Star | TokenKind::Slash => 20,
-            TokenKind::Cap => 30,
-            _ => panic!("Numbers can't have binding power!"),
-        }
-    }
-
-    /// Return true if the operator is left-associative.
-    pub fn is_left_associative(&self) -> bool {
-        match self.kind {
-            TokenKind::Plus | TokenKind::Minus | TokenKind::Slash | TokenKind::Star => true,
-            TokenKind::Cap => false,
-            _ => panic!("NUmbers can't be left (or right) associative!"),
         }
     }
 }
